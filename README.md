4867c5b8f455b571ea3648ad5ad14e071e9ea93f
alistairwalsh <alistair.walsh@me.com>
2015-11-04 12:30:58 +1100

    clean up

--- README.md
+++
@@ -1,2 +0,0 @@
-# Psychopy Eye Tracking
-Psychopy experiment with Eyelink eyetracking with pylink library and Datapixx2 hardware.
---
+++ pylink_test.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  4 08:25:05 2015
+
+@author: Peter Goodin
+@author: Alistair Walsh
+"""
+
+from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
+from psychopy import visual, core, data, logging, sound, gui, parallel
+
+#Pylink imports
+from pylink import *
+from pygame import *
+
+from psychopy.constants import *  # things like STARTED, FINISHED
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import sin, cos, tan, log, log10, pi, average, sqrt, std, deg2rad, rad2deg, linspace, asarray
+from numpy.random import random, randint, normal, shuffle
+
+import os  # handy system and path functions
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+expName = 'fb_task'  # from the Builder filename that created this script
+expInfo = {'participant':'', 'session':'001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False: core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + 'data/%s_%s_%s' %(expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath=None,
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+#save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+
+#Makes vairable to pass to eyetracker EDF file
+#NOTE: MAX 4 characters (including .edf)
+subj=str(expInfo['participant'])
+
+                    ####BEGIN EYELINK CODE####
+
+#Makes an instance of the eyetracker to interact with
+tracker = EyeLink()
+
+#Gets display information so it doesn't need to be manually set 
+#NOTE: Change this if specific resolution is required. 
+disp = getDisplayInformation()
+
+                ###START EYETRACKER CALIBRATION###
+
+#Opens a window for the eyetracker to display graphics on. 
+display.init()
+#display.set_mode((disp.width, disp.height), FULLSCREEN |DOUBLEBUF,32)
+display.set_mode((800,600), FULLSCREEN)
+mouse.set_visible(False)
+openGraphics()
+
+#Opens the EDF file.
+edfFileName = subj + ".EDF";
+tracker.openDataFile(edfFileName)		
+	
+pylink.flushGetkeyQueue(); 
+tracker.setOfflineMode();                          
+
+#Gets the display surface and sends a message to EDF file;
+surf = display.get_surface()
+tracker.sendCommand("screen_pixel_coords =  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
+tracker.sendMessage("DISPLAY_COORDS  0 0 %d %d" %(surf.get_rect().w - 1, surf.get_rect().h - 1))
+
+tracker_software_ver = 0
+eyelink_ver = tracker.getTrackerVersion()
+if eyelink_ver == 3:
+	tvstr = tracker.getTrackerVersionString()
+	vindex = tvstr.find("EYELINK CL")
+	tracker_software_ver = int(float(tvstr[(vindex + len("EYELINK CL")):].strip()))
+	
+
+if eyelink_ver>=2:
+	tracker.sendCommand("select_parser_configuration 0")
+	if eyelink_ver == 2: #turn off scenelink camera stuff
+		tracker.sendCommand("scene_camera_gazemap = NO")
+else:
+	tracker.sendCommand("saccade_velocity_threshold = 35")
+	tracker.sendCommand("saccade_acceleration_threshold = 9500")
+ 
+tracker.sendCommand("file_sample_data  = LEFT,GAZE,AREA,GAZERES,STATUS,INPUT")
+ 
+setCalibrationColors((255, 255, 255), (0, 0, 0));  	#Sets the calibration target and background color
+setTargetSize(int(surf.get_rect().w/70), int(surf.get_rect().w/300));	#select best size for calibration target
+setCalibrationSounds("", "", "");
+setDriftCorrectSounds("", "off", "off");
+
+tracker.doTrackerSetup()
+
+closeGraphics()
+
+msecDelay(1000)
+
+                    ###PASTE PSYCHOPY SCRIPT BELOW###
+# Start Code - component code to be run before the window creation
+
+# Setup the Window
+win = visual.Window(size=(1440, 900), fullscr=True, screen=0, allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True,
+    )
+# store frame rate of monitor if we can measure it successfully
+expInfo['frameRate']=win.getActualFrameRate()
+if expInfo['frameRate']!=None:
+    frameDur = 1.0/round(expInfo['frameRate'])
+else:
+    frameDur = 1.0/60.0 # couldn't get a reliable measure so guess
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+ISI = core.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='ISI')
+text = visual.TextStim(win=win, ori=0, name='text',
+    text=u'hi how are you?\nplease press the space bar',    font=u'Arial',
+    pos=[0, 0], height=0.1, wrapWidth=None,
+    color=u'white', colorSpace='rgb', opacity=1,
+    depth=-1.0)
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+#------Prepare to start Routine "trial"-------
+t = 0
+trialClock.reset()  # clock 
+frameN = -1
+# update component parameters for each repeat
+key_resp_2 = event.BuilderKeyResponse()  # create an object of type KeyResponse
+key_resp_2.status = NOT_STARTED
+# keep track of which components have finished
+trialComponents = []
+trialComponents.append(ISI)
+trialComponents.append(text)
+trialComponents.append(key_resp_2)
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+#-------Start Routine "trial"-------
+continueRoutine = True
+while continueRoutine:
+    # get current time
+    t = trialClock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text* updates
+    if t >= 0.0 and text.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        text.tStart = t  # underestimates by a little under one frame
+        text.frameNStart = frameN  # exact frame index
+        text.setAutoDraw(True)
+    
+    # *key_resp_2* updates
+    if t >= 0.0 and key_resp_2.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        key_resp_2.tStart = t  # underestimates by a little under one frame
+        key_resp_2.frameNStart = frameN  # exact frame index
+        key_resp_2.status = STARTED
+        # keyboard checking is just starting
+        win.callOnFlip(key_resp_2.clock.reset)  # t=0 on next screen flip
+        event.clearEvents(eventType='keyboard')
+    if key_resp_2.status == STARTED:
+        theseKeys = event.getKeys(keyList=['space'])
+        
+        # check for quit:
+        if "escape" in theseKeys:
+            endExpNow = True
+        if len(theseKeys) > 0:  # at least one key was pressed
+            key_resp_2.keys = theseKeys[-1]  # just the last key pressed
+            key_resp_2.rt = key_resp_2.clock.getTime()
+            # a response ends the routine
+            continueRoutine = False
+    # *ISI* period
+    if t >= 0.0 and ISI.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        ISI.tStart = t  # underestimates by a little under one frame
+        ISI.frameNStart = frameN  # exact frame index
+        ISI.start(0.5)
+    elif ISI.status == STARTED: #one frame should pass before updating params and completing
+        ISI.complete() #finish the static period
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+#-------Ending Routine "trial"-------
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# check responses
+if key_resp_2.keys in ['', [], None]:  # No response was made
+   key_resp_2.keys=None
+# store data for thisExp (ExperimentHandler)
+thisExp.addData('key_resp_2.keys',key_resp_2.keys)
+if key_resp_2.keys != None:  # we had a response
+    thisExp.addData('key_resp_2.rt', key_resp_2.rt)
+thisExp.nextEntry()
+# the Routine "trial" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+win.close()
+core.quit()
